ca8cda58ca16ee828a36f99d8a756bb74887e53c
Nik_Fuchs <nik.fuchs@me.com>
2025-09-29 10:18:24 +0200

    Aufgabe 6

---
+++ Aufgabe_06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aufgabe_06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        int n = 1;
+        int Number = 0;
+        while (Number <= 99)
+        {
+            Number = i * n;
+            i++;
+            Console.Write($"{Number}");
+            Console.Write('\t');
+        }
+        
+    }
+}
