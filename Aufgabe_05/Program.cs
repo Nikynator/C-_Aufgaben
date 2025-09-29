df2eb8828b23263e2701f774e5a7abc5897b26d5
Nik_Fuchs <nik.fuchs@me.com>
2025-09-29 09:44:16 +0200

    Aufgabe 5

---
+++ Aufgabe_05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aufgabe_05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Wie viele Kilometer möchtest du rennen? ");
+        string eingabe1 = Convert.ToString(Console.ReadLine());
+        int Zahl = Convert.ToInt16(eingabe1);
+        if (Zahl > 42)
+        {
+            Console.WriteLine("Das schaffst du nicht!");
+        }
+        else
+        {
+            int AnzahlBahnrunden = Zahl * 1000 / 400;
+            Console.Write($"Das sind {AnzahlBahnrunden} Runden. Bereit für den Lauf?(ja/nein)");
+            string eingabe2 = Convert.ToString(Console.ReadLine());
+            if (eingabe2 == "ja")
+            {
+                int i = 0;
+                while (i <= Zahl)
+                {
+                    Console.WriteLine($"Du läufst Runde {i}.");
+                    i++;
+                }
+                Console.WriteLine("Du hast es geschafft");
+            }
+        }
+    }
+}
