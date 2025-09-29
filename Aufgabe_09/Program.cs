da0fab7eb4fdefae7bbfe8edaf91f7c2ac2c605d
Nik_Fuchs <nik.fuchs@me.com>
2025-09-29 16:34:36 +0200

    Aufgabe 9 bis zum Feierabend

---
+++ Aufgabe_09/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Aufgabe_09;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Number:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string bin = ("");
+        int rest;
+        int werte;
+        while (n != 0)
+        {
+            rest = n % 2;
+            bin = rest + bin;
+            werte = n / 2;
+            n = werte;
+            if (n==0)
+            {
+                Console.WriteLine(bin);
+            }
+        }
+        bool kaypressed = false;
+        ConsoleKeyInfo keyInfo = Console.ReadKey();
+        if (keyInfo.KeyChar == 'q')
+        {
+            kaypressed = true;
+            Console.WriteLine("Your done buddy boy");
+        }
+        else
+        {
+            kaypressed = false;
+        }
+    }
+}
