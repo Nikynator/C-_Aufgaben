374b40e0cfce2aa3c5c99165e1a7881198c11ae8
Nik_Fuchs <nik.fuchs@me.com>
2025-09-29 14:54:55 +0200

    Aufgabe 8

---
+++ Aufgabe_08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Aufgabe_08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dein Kommentar:");
+        string eingabe = Console.ReadLine();
+        string final = eingabe.ToLower();
+        string[] forbiddenWords = { "viagra", "sex", "porno", "fick", "schlampe", "arsch" };
+        string[] EingabeSplit = final.Split(' ');
+        int i = 0;
+        foreach (string wort in forbiddenWords)
+        {
+            if (EingabeSplit.Contains(wort))
+            {
+                i++;
+                //Console.WriteLine($"Das Wort: '{wort}' wurde in der Benutzereingabe gefunden.");
+                Console.WriteLine($"There are {i} violations.");
+            }
+        }
+    }
+}
