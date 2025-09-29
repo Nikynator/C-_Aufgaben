4f2f238189c1a243698627269cb14d9f0ef4cc79
Nik_Fuchs <nik.fuchs@me.com>
2025-09-29 09:25:26 +0200

    Fix Aufgabe 2 und Aufgabe 4

--- Aufgabe_02/Program.cs
+++ Aufgabe_02/Program.cs
@@ -4,18 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Berechnung von Sekunden eines Monates in Abhängigkeit seiner Anzahl Tage");
+        Console.WriteLine("Berechnung von Sekunden eines Monates in Abhängigkeit seiner Anzahl Tage:");
         Console.Write("Wie viele Tage hat der Monat, für den Sie die Sekundenzahl brechnen wollen?");
-        string eingabe1 = Console.ReadLine();
-        int Zahl = Convert.ToInt16(eingabe1);
-        if (Zahl > 27 && Zahl < 32)
+        string eingabe1 = Convert.ToString(Console.ReadLine());
+        int Zahl = 0;
+        if (int.TryParse(eingabe1, out Zahl))
         {
-            int FinalNumber = Zahl * 24 * 60 * 60;
-            Console.WriteLine($"Ein Monat mit {eingabe1} hat {FinalNumber}.");
+            int Zahl1 = Convert.ToInt16(eingabe1);
+            if (Zahl1 > 27 && Zahl1 < 32)
+            {
+                int FinalNumber = Zahl1 * 24 * 60 * 60;
+                Console.WriteLine($"Ein Monat mit {eingabe1} hat {FinalNumber}.");
+            }
+            else
+            {
+                Console.WriteLine($"Ungültige Eingabe. Ein Monate mit {eingabe1} gibt es nicht.");
+            }
         }
         else
         {
-            Console.WriteLine($"Ungültige Eingabe. Ein Monate mit {Zahl} gibt es nicht.");
+            Console.WriteLine($"Eingabefehler. Bitte geben Sie eine Zahl valide ein.");
         }
     }
 }
---
+++ Aufgabe_04/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Aufgabe_04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Zahl eingeben: ");
+        string eingabe = Console.ReadLine();
+        
+        int Zahl = 0;
+        string[] Months = new string[12]; 
+        Months[0] = "Januar";
+        Months[1] = "Februar";
+        Months[2] = "März";
+        Months[3] = "April";
+        Months[4] = "Mai";
+        Months[5] = "Juni";
+        Months[6] = "Juli";
+        Months[7] = "August";
+        Months[8] = "September";
+        Months[9] = "October";
+        Months[10] = "November";
+        Months[11] = "December";
+        if (int.TryParse(eingabe, out Zahl))
+        {
+            int i = Convert.ToInt16(eingabe);
+            if (i >= 1 && i <= 12)
+            {
+                i = i - 1;
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine(Months[i]);
+            }
+            else
+            {
+                Console.WriteLine("Your Number isn't valid please choose a Number between 1 and 12.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Your Number isn't valid please choose a Absolut Number.");
+        }
+    }
+}
