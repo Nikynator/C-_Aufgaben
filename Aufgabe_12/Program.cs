225c18d754fae7706594495cd6b7ba4bec7683f2
Nik_Fuchs <nik.fuchs@me.com>
2025-09-30 16:23:10 +0200

    Aufgabe 12 mit Methode

--- Aufgabe_12/Program.cs
+++ Aufgabe_12/Program.cs
@@ -1,25 +1,38 @@
-﻿Console.WriteLine("Geben Sie die zu summierenden Ganzzahlen mit Komma getrennt ein: ");
-Console.WriteLine(" ");
-string input = Console.ReadLine();
-string[] stringArray  = input.Split(',');
-int[] intArray = Array.ConvertAll(stringArray, int.Parse);
-int sum = 0;
-        
-for (int i = 0; i < intArray.Length; i++)
-{
-    while (i <= intArray.Length - 1)
+﻿namespace Aufgabe_12;
+
+class Program{
+    static void Main(string[] args)
     {
-        sum = intArray[i] + sum;
-                
-        if (i == intArray.Length - 1)
+        Console.WriteLine("Geben Sie die zu summierenden Ganzzahlen mit Komma getrennt ein: ");
+        Console.WriteLine(" ");
+        string input = Console.ReadLine();
+        string[] stringArray = input.Split(',');
+        int[] intArray = Array.ConvertAll(stringArray, int.Parse);
+        int sum = 0;
+        methodSummieren(sum, intArray);
+    }
+
+
+    static int methodSummieren(int sum, int[] intArray)
+    {
+        for (int i = 0; i < intArray.Length; i++)
         {
-            Console.Write("[" + i + "]" + " -> " + sum);
-            i++;
+            while (i <= intArray.Length - 1)
+            {
+                sum = intArray[i] + sum;
+
+                if (i == intArray.Length - 1)
+                {
+                    Console.Write("[" + i + "]" + " -> " + sum);
+                    i++;
+                }
+                else
+                {
+                    Console.Write("[" + i + "]" + " -> " + sum + ", ");
+                    i++;
+                }
+            }
         }
-        else
-        {
-            Console.Write("[" + i + "]" + " -> " + sum + ", ");
-            i++;
-        }
+        return sum;
     }
 }
---
+++ Aufgabe_16/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aufgabe_16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
