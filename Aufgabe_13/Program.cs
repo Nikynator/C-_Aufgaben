8c324f3cd44103e2f2a7e95df0fb6f02a4c42a32
Nik_Fuchs <nik.fuchs@me.com>
2025-09-30 12:48:49 +0200

    Aufgabe 13

---
+++ Aufgabe_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Geben Sie die länge der Linie ein: ");
+Console.WriteLine(" ");
+string input = Console.ReadLine();
+int intInput = Convert.ToInt32(input);
+
+string [,] stringArray = new string [intInput, intInput];
+for (int i = 0; i < intInput; i++)
+{
+    stringArray[i,i] = " ";
+}
+
+for (int zeile = 0; zeile < stringArray.GetLength(0); zeile++)
+{
+    for (int spalte = 0; spalte < stringArray.GetLength(1); spalte++)
+    {
+        Console.Write(stringArray[zeile, spalte] + "*");
+    }
+    Console.WriteLine();
+}
