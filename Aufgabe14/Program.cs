3c20ecf68835a74bcac81a897c5a6603c52f77e7
Nik_Fuchs <nik.fuchs@me.com>
2025-09-30 13:40:48 +0200

    Aufgabe 14

---
+++ Aufgabe14/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Aufgabe14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Prüfen, ob es sich bei einem Jahr um ein Schaltjahr handelt.");
+        Console.WriteLine("-------------------------------------------------------------");
+        
+        bool keypressed = false;
+        while (keypressed == false)
+        {
+            string bin = (" ");
+            int userNumber;
+            int rest;
+            int value;
+
+        Console.Write("Eingabe Jahr (q to quit): ");
+        int InputInt = Convert.ToInt32(Console.ReadLine());
+        string input = InputInt.ToString();
+
+            if (input == "q")
+            {
+                keypressed = true;
+                Console.WriteLine("You pressed q which stopped the programm");
+            }
+            
+        }
+        if (InputInt % 4 == 0)
+        {
+            if (InputInt % 400 == 0)
+            {
+                Console.WriteLine($"Das Jahr {input} ist ein Schaltjahr");
+            }
+            else
+            {
+                Console.WriteLine($"Das Jahr {input} ist KEIN Schaltjahr");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Das Jahr {input} ist KEIN Schaltjahr");
+        }
+
+    }
+}
---
+++ Aufgabe_14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.Design;
+
+namespace Aufgabe_14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Check if a year is a leap year");
+        Console.WriteLine("******************************");
+        
+        
+        bool keypressed = false;
+
+        while (keypressed == false)
+        {
+            int year;
+            string input;
+
+            Console.WriteLine("\nEnter Year (q + enter to Quit): ");
+            input = Console.ReadLine();
+
+
+            if (input == "q")
+            {
+                keypressed = true;
+                Console.WriteLine("You pressed q which stopped the programm");
+            }
+            else
+            {
+                year = Convert.ToInt32(input);
+                keypressed = false;
+                
+                if (year % 4 == 0)
+                {
+                    if (year % 100 == 0)
+                    {
+                        if (year % 400 == 0)
+                        {
+                        }
+                        else
+                        {
+                            Console.WriteLine("This isn't a leap year!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("This is a leap year!");
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("This isn't a leap year!");
+                }
+
+            }
+        }
+    }
+}
