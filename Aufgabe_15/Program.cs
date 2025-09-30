db0f90edab433581e8b6204d7a747fa168691d60
Nik_Fuchs <nik.fuchs@me.com>
2025-09-30 13:47:48 +0200

    First Steps Aufgabe 15

---
+++ Aufgabe_15/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aufgabe_15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Breites des Stammes: ");
+        string inputBreite = Console.ReadLine();
+        int intInputBreite = Convert.ToInt16(inputBreite);
+        Console.Write("Höhe des Stammes: ");
+        string inputHöhe = Console.ReadLine();
+        int intInputHöhe = Convert.ToInt16(inputHöhe);
+        Console.Write("Höhe der Krone: ");
+        
+    }
+}
