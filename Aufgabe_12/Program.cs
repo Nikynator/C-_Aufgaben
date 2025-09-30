e7be0c931c8f350f4799d510cf0ca9ebbf38e583
Nik_Fuchs <nik.fuchs@me.com>
2025-09-30 11:10:12 +0200

    Aufgabe 12

---
+++ Aufgabe_12/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Geben Sie die zu summierenden Ganzzahlen mit Komma getrennt ein: ");
+Console.WriteLine(" ");
+string input = Console.ReadLine();
+string[] stringArray  = input.Split(',');
+int[] intArray = Array.ConvertAll(stringArray, int.Parse);
+int sum = 0;
+        
+for (int i = 0; i < intArray.Length; i++)
+{
+    while (i <= intArray.Length - 1)
+    {
+        sum = intArray[i] + sum;
+                
+        if (i == intArray.Length - 1)
+        {
+            Console.Write("[" + i + "]" + " -> " + sum);
+            i++;
+        }
+        else
+        {
+            Console.Write("[" + i + "]" + " -> " + sum + ", ");
+            i++;
+        }
+    }
+}
