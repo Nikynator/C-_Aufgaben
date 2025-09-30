d78174d8b55bfbd81f28e557520dcc9a4a152852
Nik_Fuchs <nik.fuchs@me.com>
2025-09-30 08:50:27 +0200

    Aufgabe 10

---
+++ Aufgabe_10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aufgabe_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Zahl:");
+        int zahl = Convert.ToInt32(Console.ReadLine());
+        int sum = BerechneQuersumme(zahl);
+        Console.WriteLine(sum);
+    }
+
+    static int BerechneQuersumme(int zahl)
+    {
+        int sum = 0;
+        while (zahl != 0)
+        {
+            sum = sum + (zahl % 10);
+            zahl = zahl / 10;
+        }
+
+        return sum;
+    }
+}
